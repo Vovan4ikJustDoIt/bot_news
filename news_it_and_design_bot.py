97a6a2511b2eca8e426b4789a89123c783f8f4d9
Volodymyr Kovtun <jxallkmen@ukr.net>
2024-11-14 23:52:10 +0200

    Edit RSS 3 and chat_id

--- news_it_and_design_bot.py
+++ news_it_and_design_bot.py
@@ -16,16 +16,13 @@ logger = logging.getLogger(__name__)
 TOKEN = '7846084048:AAH5TLUctSLeeVo3liWh-rmB5KsLwWh1_X8'
 
 # Чат ID групи для публікації новин
-CHAT_ID = '590241563'
+CHAT_ID = '-1002332086987'
 
 # URL-адреси RSS-каналів
 RSS_FEEDS = [
     'http://k.img.com.ua/rss/ua/web.xml',
     'https://itc.ua/ua/feed/',
     'https://hi-tech.ua/uk/tag/shtuchnyj-intelekt/feed/',
-    'http://k.img.com.ua/rss/ua/technews.xml',
-    'http://k.img.com.ua/rss/ua/interiors.xml',
-    'https://ukranews.com/ua/rss-gen/Sub=323',
     #'https://rss.it.ua/feed.xml',          # IT новини
     #'https://rss.designnews.com/feed.xml'   # Новини дизайну
 ]
@@ -35,7 +32,7 @@ def get_news():
     news_list = []
     for feed_url in RSS_FEEDS:
         feed = feedparser.parse(feed_url)
-        for entry in feed.entries[:1]:  # Обмеження до 5 новин
+        for entry in feed.entries[:1]:  # Обмеження до 1 новини
             title = entry.title
             link = entry.link
             news_list.append(f"{title}\n{link}")
